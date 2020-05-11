17f4e130b5f08740dff85050975b19eddb9b1554
ankurkhetan2015 <ankurkhetan2615@gmail.com>
2020-05-10 22:14:16 -0500

    Added reset and lap functionalities, added lap details

--- js/stopwatch.js
+++ js/stopwatch.js
@@ -12,6 +12,12 @@ $(function()
 	var lapNum = 0;
 	//minutes, seconds and cseconds for time, lap
 	var timeMin, timeSec, timeCsec, lapMin, lapSec, lapCsec;
+
+	//last lap time to keep aggregate time
+	var aggMin = 0;
+	var aggSec = 0;
+	var aggCsec = 0;
+	var aggTotal = 0;	
 	
 	//On App load show only start and lap buttons
 	hideshowButtons("#startbutton", "#lapbutton");
@@ -55,9 +61,23 @@ $(function()
 	});
 
 	//click on lap button
+	$("#lapbutton").click(function()
+	{
 		//if counter already started
-			//reset lap and show the lap details
+		if(mode)
+		{
+			clearInterval(increment);
+			//reset lap
+			lapCounter = 0;
 
+			//show the lap details
+			addLap()
+
+			//start the watch again
+			startWatch();
+		}
+	});
+	
 	//shows current required two buttons
 	function hideshowButtons(x, y)
 	{
@@ -118,4 +138,39 @@ $(function()
 			return number;
 		}
 	}
+
+	//evaluates and display lap details
+	function addLap()
+	{
+		lapNum++;
+
+		aggMin += lapMin;
+		aggSec += lapSec;
+		aggCsec += lapCsec;
+
+		aggTotal = aggCsec + aggSec * 100 + aggMin * 6000;
+
+		aggMin = Math.floor(aggTotal / 6000);
+		aggSec = Math.floor((aggTotal % 6000) / 100);
+		aggCsec = (aggTotal % 6000) % 100;
+
+		var lapDetails = 
+		'<div class="lap">'+
+			
+			'<div class="lapTitle">' + 
+				'Lap' + format(lapNum) +
+			'</div>' +
+
+			'<div class="lapIndividual">' + 
+				'<span>' + format(lapMin) + '</span>' + ':<span>' + format(lapSec) + '</span>' + ':<span>' + format(lapCsec) + '</span>' +
+			'</div>' +
+
+			'<div class="lapAggregate">' +
+			 	'<span>' + format(aggMin) + '</span>' + ':<span>' + format(aggSec) + '</span>' + ':<span>' + format(aggCsec) + '</span>' +
+			'</div>' +
+
+		'</div>';
+		$(lapDetails).prependTo("#diffLaps");
+	}
+	
 });
